953ad547bc381257ac9ef33d38cb4993c73bddb0
Anuj Pokhrel <pokhrel.anuz7@gmail.com>
2025-07-21 16:38:19 -0400

    first comit

---
+++ damage_trial.py
@@ -0,0 +1,59 @@
+from time import sleep
+
+from beamngpy import BeamNGpy, Scenario, Vehicle, set_up_simple_logging
+from beamngpy.sensors import Mesh, Damage
+
+def main():
+    set_up_simple_logging()
+
+    #set up the simulator
+    bng = BeamNGpy("localhost", 25252, home='/home/robotixx/Downloads/BeamNG.tech.v0.36.4.0')
+    bng.open(launch=True)
+
+    #create a vehicle
+    vehicle = Vehicle("ego_vehicle", model="etk800", license="DAMAGED", color="Red")
+    vehicle.attach_sensor('damage', Damage())
+    #create a scenario
+    scenario = Scenario("industrial", "broken_mesh_test", description="Mesh analysis")
+    # Add the vehicle to the scenario
+    scenario.add_vehicle(
+        vehicle, pos=(408.36, -336.56, 35.54), rot_quat=(0,0, 0.99, -0.17)
+    )
+    scenario.make(bng)
+
+    # Set simulator to 60Hz temportal resolution
+    bng.settings.set_deterministic(60)
+    bng.scenario.load(scenario)
+    # bng.ui.hide_hud()
+    bng.scenario.start()
+
+    bng.step(60)
+
+    vehicle.send_damage([
+        ('front_left_suspension', 1.0, (0,0,0)),
+    ])
+
+    #Get the mesh data for the vehicle.
+    mesh = Mesh("mesh1", bng, vehicle)
+
+    print("Driving around, polling the mesh data every 5 seconds....")
+    vehicle.ai.set_mode("traffic")
+
+    for _ in range(3):
+        sleep(5)
+        data = mesh.poll()
+        print("data time:", data["time"])
+        mesh.mesh_plot()
+        mesh.mass_distribution_plot(data['nodes'])
+        mesh.force_distribution_plot(data['nodes'])
+        mesh.force_direction_plot(data['nodes'])
+        mesh.velocity_direction_plot(data['nodes'])
+        mesh.velocity_distribution_plot(data['nodes'])
+    
+    vehicle.ai.set_mode("disabled")
+    bng.ui.show_hud()
+    input("Press Enter to exit....")
+    bng.disconnect()
+
+if __name__ == "__main__":
+    main()
